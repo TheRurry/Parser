24b2f6906c2ba475952ef6413856dfca004648b5
Ryan Collins <zcabrdc@ucl.ac.uk>
2015-12-23 23:24:21 +0000

    Added evaluation for bin fmla

--- modelChecker.c
+++ modelChecker.c
@@ -54,10 +54,8 @@ int findBin(char *g) {
 int valid(char *g) {
   int binPos;
   switch(*g) {
-    case 'X':
-      return atom(g);
-    case '-':
-      return valid(substr(g, 1, strlen(g) - 1));
+    case 'X': return atom(g);
+    case '-': return valid(substr(g, 1, strlen(g) - 1));
     case '(':
       binPos = findBin(g);
       if (binPos == 0)
@@ -87,13 +85,45 @@ int parse(char *g) {
   }
 }
 
+int eval();
+
+//Checks to see if an atomic fmla will evaluate tree
+int checkAtom(char *nm, int edges[no_edges][2], int V[3]) {
+  int var1 = 0, var2 = 0, i;
+  for (i = 0; i < 3; i++) {
+    if (*(nm+2) == 'x' + i)
+      var1 = V[i];
+    if (*(nm+3) == 'x' + i)
+      var2 = V[i];
+  }
+  for (i = 0; i < no_edges; i++) {
+    if (var1 == edges[i][0] && var2 == edges[i][1])
+      return 1;
+  }
+  return 0;
+}
+
+int checkBin(char *nm, int edges[no_edges][2], int size, int V[3]) {
+  int binPos = findBin(nm);
+  char binConn = *(nm + binPos);
+  int lFmla = eval(substr(nm, 1, binPos-1), edges, size, V);
+  int rFmla = eval(substr(nm, binPos+1, strlen(nm)-2), edges, size, V);
+  if (binConn == 'v' && (lFmla && rFmla))
+    return 1;
+  if (binConn == '^' && (lFmla || rFmla))
+    return 1;
+  if (binConn == '>' && !(lFmla && !rFmla))
+    return 1;
+  return 0;
+}
+
 //this method takes a formula, the list of edges of a graph, the number of vertices and a variable assignment. It then evaluates the formula and returns 1 or 0 as appropriate.
-int eval(char *nm, int edges[no_edges][2], int size, int V[3])
-
-{
+int eval(char *nm, int edges[no_edges][2], int size, int V[3]) {
 	switch(parse(nm)) {
-    case 1:
-
+    case 1: return checkAtom(nm, edges, V);
+    case 2: return !eval(substr(nm, 1, strlen(nm) - 1), edges, size, V);
+    case 3: return checkBin(nm, edges, size, V);
+    default: return 0; break;
 	}
 }
 
@@ -105,14 +135,14 @@ int main()
   printf("Enter a formula: ");
   scanf("%s", name);
   int p=parse(name);
-  switch(p)
-    {case 0: printf("Not a formula"); break;
+  switch(p) {
+    case 0: printf("Not a formula"); break;
     case 1: printf("An atomic formula"); break;
     case 2: printf("A negated formula"); break;
     case 3: printf("A binary connective formula"); break;
     case 4: printf("An existential formula"); break;
     case 5: printf("A universal formula"); break;
-    default: printf("Not a formula"); return 0;
+    default: printf("Not a formula"); break;
     }
 
   //Input a graph.  No. of nodes, edges, then input each edge.
@@ -125,7 +155,7 @@ int main()
 
   int edges[no_edges][2];  //Store edges in 2D array
   int  i, k, j;
-  for(i=0;i<no_edges;i++) {
+  for(i=0;i < no_edges;i++) {
     printf("input a pair of nodes (<%d)", no_nodes);
     scanf(" %d", &j);scanf(" %d", &k);
     edges[i][0]=j; edges[i][1]=k;
@@ -143,9 +173,10 @@ int main()
   printf("z is ?");scanf(" %d", &V[2]);
 
   /*Now check if formula is true in the graph with given variable assignment. */
-  if (eval(name, edges, no_nodes,  V)==1)
-    printf("The formula %s is true", name); else printf("The formula %s is false", name);
-
+  if (eval(name, edges, no_nodes, V)==1)
+    printf("The formula %s is true", name);
+  else
+    printf("The formula %s is false", name);
   free(name);
   return 0;
 }
